8fd3a9a4faffba18ed2f516f4015efddc7bfc41c
Jonathan De La Cruz <jonathandelacruzalvarez@gmail.com>
2024-10-03 13:24:16 -0600

    Eliminando node_modules del repositorio

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html', '*.js'], // Ajusta las rutas según tus archivos
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
